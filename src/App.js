22a784970ce87cb6c4d3981963dd7e5426c7cf36
vgk <vogiakhang1507@gmail.com>
2022-08-30 15:36:40 +0700

    delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
+        <h1>Hello world - VGK</h1>
       </header>
     </div>
   );
