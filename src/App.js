1afcbd7655d40df86a5bc15e67797e2763aa23bf
vgk <vogiakhang1507@gmail.com>
2022-08-31 15:10:40 +0700

    handle event onClick

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import "./App.css";
 import Nav from "./Views/Nav";
 
 function App() {
+  const handleEventClick = (event) => {
+    console.log("click me", event.target.value);
+  };
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world - VGK</h1>
+        <input
+          type="text"
+          value={"VGK"}
+          onClick={(event) => handleEventClick(event)}
+        />
+        <button type="button" onClick={(event) => handleEventClick(event)}>
+          click me
+        </button>
       </header>
     </div>
   );
