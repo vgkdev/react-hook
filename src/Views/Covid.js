87c16b287b2f7fd86b6189c1ca3ef94f8fdd46b2
vgk <vogiakhang1507@gmail.com>
2022-09-02 13:55:03 +0700

    handle loading data

--- src/Views/Covid.js
+++ src/Views/Covid.js
@@ -4,22 +4,27 @@ import moment from "moment";
 
 const Covid = () => {
   const [dataCovid, setDataCovid] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      let response = await axios.get(
-        "https://api.covid19api.com/country/vietnam?from=2021-11-01T00%3A00%3A00Z&to=2021-11-20T00%3A00%3A00Z"
-      );
-      let data = response && response.data ? response.data : [];
-      if (data && data.length > 0) {
-        data.map((item) => {
-          item.Date = moment(item.Date).format("DD/MM/YYYY");
-          return item;
-        });
-      }
-      setDataCovid(data);
-      //console.log("check response: ", response.data);
+      setTimeout(async () => {
+        let response = await axios.get(
+          "https://api.covid19api.com/country/vietnam?from=2021-11-01T00%3A00%3A00Z&to=2021-11-20T00%3A00%3A00Z"
+        );
+        let data = response && response.data ? response.data : [];
+        if (data && data.length > 0) {
+          data.map((item) => {
+            item.Date = moment(item.Date).format("DD/MM/YYYY");
+            return item;
+          });
+          data = data.reverse();
+        }
+        setDataCovid(data);
+        setLoading(false);
+      }, 2000);
     };
+
     fetchData();
   }, []);
 
@@ -38,7 +43,8 @@ const Covid = () => {
           </tr>
         </thead>
         <tbody>
-          {dataCovid &&
+          {loading === false &&
+            dataCovid &&
             dataCovid.length > 0 &&
             dataCovid.map((item) => {
               return (
@@ -51,6 +57,14 @@ const Covid = () => {
                 </tr>
               );
             })}
+
+          {loading === true && (
+            <tr>
+              <td colSpan={5} style={{ textAlign: "center" }}>
+                Loading...
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </>
